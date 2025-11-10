a713a3fd46e25b9bbcafe712002f1708da1ae4e4
Henry Zhang <zeri@umich.edu>
2025-11-10 12:36:21 -0500

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+
+def main():
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
